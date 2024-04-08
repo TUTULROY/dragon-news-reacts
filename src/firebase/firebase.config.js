c8b285fa3c37f9d4dd5083afb43f8a1cb558c5a5
TUTULROY <tutulroyjoy@gmail.com>
2024-04-08 10:00:29 +0600

    modified firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC551lMx0kIkjp5p4f8TzoQTjl5ZFT4b9c",
-  authDomain: "dragon-news-reacts.firebaseapp.com",
-  projectId: "dragon-news-reacts",
-  storageBucket: "dragon-news-reacts.appspot.com",
-  messagingSenderId: "1074057617706",
-  appId: "1:1074057617706:web:55e64642b6c1834394be53"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId:import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../../Shared/Navber/Navbar";
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProvider";
@@ -7,6 +7,9 @@ import { AuthContext } from "../../providers/AuthProvider";
 const Login = () => {
 
     const {signIn} = useContext(AuthContext)
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log('location in here', location)
 
 const handleLogin = e =>{
 
@@ -20,6 +23,9 @@ const handleLogin = e =>{
     signIn(email, password)
     .then(result => {
         console.log(result.user)
+
+            navigate(location?.state ? location.state : "/");
+
     })
     .catch(error => console.error(error))
 }
